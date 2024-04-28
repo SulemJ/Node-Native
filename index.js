7a20e93305831d4075945abd8799043dc980c7b0
SulemJ <sulemjibrildawud@gmail.com>
2024-04-28 12:26:00 +0300

    initial commit

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const fs = require("fs");
-//  fs.writeFile("massage.txt", "hello susu!", (err) => {
-//     if (err) throw err;
-//     console.log('The file has been saved!');
-//   });  
+ fs.writeFile("massage.txt", "hello susu!", (err) => {
+    if (err) throw err;
+    console.log('The file has been saved!');
+  });  
  
 
   fs.readFile('./massage.txt',  'utf8', (err, data) => {
